1a0d33e92f5cba64a605d7a4f721808597d0919b
Adnan Dzelihodzic <adnandz@gmail.com>
2019-04-16 12:01:28 +0200

    swagger

--- AspNetCoreAPI/Startup.cs
+++ AspNetCoreAPI/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace AspNetCoreAPI
 {
@@ -31,6 +32,11 @@ namespace AspNetCoreAPI
             services.AddDbContext<MVCContext>(options =>
               options.UseSqlServer(Configuration.GetConnectionString("AzureConnection")));
 
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,7 +51,16 @@ namespace AspNetCoreAPI
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-            
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
+
+
             app.UseHttpsRedirection();
             app.UseMvc();
         }
